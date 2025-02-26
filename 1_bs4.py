252aa86ad96f83be134441f328788aed8e12eda1
Vaibhav Desai <97426964+Monster682@users.noreply.github.com>
2025-02-26 23:48:07 +0530

    Update 1_bs4.py

--- 1_bs4.py
+++ 1_bs4.py
@@ -1,11 +1,1 @@
-from bs4 import BeautifulSoup
 
-with open("flipkart.html","r") as f:
-    html = f.read()
-soup = BeautifulSoup(html , 'html.parser')
-
-print("Title of the page is: ", soup.title.text)
-print("All Tags: ", soup.find_all("a"))
-print("Title string: ", soup.title.string)
-print("Title parent name: ", soup.title.parent.name)
-print("All links:", soup.find_all("a")) 
